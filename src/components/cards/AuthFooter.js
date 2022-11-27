e6739d252febf44f34b74fbdb9d60496a7a6c32d
Rajasekar T <kt.rajasekar@gmail.com>
2022-11-27 22:45:02 +0530

    First Commit by Rajasekar T

---
+++ src/components/cards/AuthFooter.js
@@ -0,0 +1,12 @@
+// material-ui
+import { useMediaQuery, Container, Link, Typography, Stack } from '@mui/material';
+
+// ==============================|| FOOTER - AUTHENTICATION ||============================== //
+
+const AuthFooter = () => {
+    const matchDownSM = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+
+    return <></>;
+};
+
+export default AuthFooter;
---
+++ src/layout/MainLayout/Drawer/DrawerContent/NavCard.js
@@ -0,0 +1,15 @@
+// material-ui
+import { Button, CardMedia, Link, Stack, Typography } from '@mui/material';
+
+// project import
+import MainCard from 'components/MainCard';
+
+// assets
+import avatar from 'assets/images/users/avatar-group.png';
+import AnimateButton from 'components/@extended/AnimateButton';
+
+// ==============================|| DRAWER CONTENT - NAVIGATION CARD ||============================== //
+
+const NavCard = () => <></>;
+
+export default NavCard;
